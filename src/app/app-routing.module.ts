65ac9886e26dc2b2ed72619430fc1bd58c3bc64b
Gourav Roy <gourav21roy@gmail.com>
2022-10-29 14:30:34 +0530

    Initial draft

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SplashComponent } from './splash/splash.component';
+import { TictactoeComponent } from './tictactoe/tictactoe.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'tictactoe', component: TictactoeComponent },
+  { path: 'home', component: SplashComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SplashComponent } from './splash/splash.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TictactoeComponent } from './tictactoe/tictactoe.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SplashComponent,
+    DashboardComponent,
+    TictactoeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
