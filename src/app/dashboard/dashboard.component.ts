8f4e8de7a2a37a7533e31d3a356469541c6ca746
Gourav Roy <gourav21roy@gmail.com>
2022-11-01 13:02:24 +0530

    Updated dashboard page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', route: ''});
+
   constructor() { }
 
   ngOnInit(): void {
+    this.slides[0] = {
+      src: './assets/ttt.png',
+      route: '/maintenance'
+    };
+    // this.slides[1] = {
+    //   src: './assets/ttt.png',
+    // }
+    // this.slides[2] = {
+    //   src: './assets/tictac.png',
+    // }
+  }
+
+  onItemChange($event: any): void {
+    console.log('Carousel onItemChange', $event);
+  }
+
+  gotoTile(tile: any){
+    console.log(tile.src);
   }
 
 }
